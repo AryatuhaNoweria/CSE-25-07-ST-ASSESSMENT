2e74b10fea2922c35e94e8a336ccfc2a90a44f13
noweria <noweriaaryatuha@gmail.com>
2025-10-23 11:58:41 +0300

    Fix custom User model: remove username, enable email/phone login

--- cse/login/models.py
+++ cse/login/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    username = None  # disable username field
     full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
